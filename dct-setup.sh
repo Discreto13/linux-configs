1599b3dea13a4172ccacfd4e05a90c93c914780c
Yegor Shashko <yegor.shashko@gmail.com>
2018-07-21 17:03:32 -0400

    dct-setup: add sublime install

--- dct-setup.sh
+++ dct-setup.sh
@@ -46,4 +46,11 @@ sudo apt-get install mysql-client mysql-workbench
 echo "Install chromium"
 sudo apt-get install chromium
 
+echo "Install sublime-text"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install sublime-text
+
 echo 'Need to reboot'
